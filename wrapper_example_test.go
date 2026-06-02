45ba8057b427233f5377e2d42efe923d3afa8ad7
Kevin Yuan <farproc@gmail.com>
2026-06-02 14:19:25 +0800

    Update wrapper_example_test.go

--- wrapper_example_test.go
+++ wrapper_example_test.go
@@ -4,36 +4,63 @@ import (
 	"github.com/mkch/gw"
 	"github.com/mkch/gw/app"
 	"github.com/mkch/gw/metrics"
+	"github.com/mkch/gw/paint"
+	"github.com/mkch/gw/paint/brush"
 	"github.com/mkch/gw/win32"
 	"github.com/mkch/gw/window"
 )
 
-// MainWindow is a Window that quits the message loop when destroyed.
+// MainWindow is the main application window.
 type MainWindow struct {
 	window.Window
+	bkColor win32.COLORREF
+	bkBrush *brush.Brush
+}
+
+func (w *MainWindow) OnInit() {
+	w.Window.OnInit()
+	// Create a solid brush with the specified background color.
+	w.bkBrush, _ = brush.New(&win32.LOGBRUSH{
+		Style: win32.BS_SOLID,
+		Color: w.bkColor,
+	})
+}
+
+func (w *MainWindow) OnPaint() {
+	dc, _ := paint.NewPaintDC(w.HWND())
+	defer func() { dc.EndPaint() }()
+	// Draw the background using the background brush.
+	win32.FillRect(dc.HDC(), dc.Rect(), w.bkBrush.HBRUSH())
 }
 
 func (w *MainWindow) OnDestroy() {
 	// Call the default implementation.
 	w.Window.OnDestroy()
+	// Release the background brush.
+	w.bkBrush.Release()
 	// Quit the message loop when this window is destroyed.
 	w.App().Quit(1)
 }
 
 // NewMainWindow creates and initializes a MainWindow instance.
-func NewMainWindow() *MainWindow {
-	return gw.Init(&MainWindow{Window: window.Window{Spec: &window.Spec{
-		X:      metrics.Dip(100),
-		Y:      metrics.Dip(100),
-		Width:  metrics.Dip(500),
-		Height: metrics.Dip(300),
-	}}})
+func NewMainWindow(title string, bkColor win32.COLORREF) *MainWindow {
+	return gw.Init(&MainWindow{
+		Window: window.Window{
+			Spec: &window.Spec{
+				Style:  win32.WS_OVERLAPPEDWINDOW,
+				Text:   title,
+				X:      gw.CW_USEDEFAULT,
+				Width:  metrics.Dip(500),
+				Height: metrics.Dip(300),
+			},
+		},
+		bkColor: bkColor,
+	})
 }
 
 func Example_wrapper() {
 	gw.Run(func(app *app.App) {
-		mainWindow := NewMainWindow()
-		mainWindow.SetText("Main Window")
+		mainWindow := NewMainWindow("Main Window", win32.RGB(128, 128, 0))
 		mainWindow.Show(win32.SW_SHOWNORMAL)
 	}, nil)
 }
